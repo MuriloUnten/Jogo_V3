f0e880169d10afacc95e20fc6481af7c5b55f457
MuriloUnten <murilokenjiunten@gmail.com>
2023-05-27 21:52:37 -0300

    GraphicsManager

---
+++ include/Managers/GraphicsManager.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#define WIDTH 1280
+#define HEIGHT 720
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+namespace Managers
+{
+	class GraphicsManager
+	{
+	private:
+		sf::RenderWindow* window;
+
+		sf::Font* font;
+
+		/* Singleton Design pattern */
+		static GraphicsManager* instance;
+		GraphicsManager();
+
+		sf::Clock clock;
+		static float deltaTime;
+
+
+	public:
+		~GraphicsManager();
+
+		static GraphicsManager* getInstance();
+		static void deleteInstance();
+
+		bool isWindowOpen();
+		void clear();
+		void close();
+		void display();
+		sf::RenderWindow* getWindow();
+
+		void updateDeltaTime();
+		static const float getDeltaTime();
+
+		sf::Font* getFont() const;
+	};
+}// namespace Managers
---
+++ src/Managers/GraphicsManager.cpp
@@ -0,0 +1,102 @@
+#include "../../include/Managers/GraphicsManager.hpp"
+
+#define FONT_PATH "../assets/Fonts/ethn.otf"
+
+namespace Managers
+{
+    GraphicsManager* GraphicsManager::instance = NULL;
+    float GraphicsManager::deltaTime = 0;
+
+    GraphicsManager* GraphicsManager::getInstance()
+    {
+        if (instance == NULL)
+        {
+            instance = new GraphicsManager();
+        }
+        return instance;
+    }
+
+
+    void GraphicsManager::deleteInstance()
+    {
+        if (instance)
+            delete instance;
+    }
+    
+    
+    GraphicsManager::GraphicsManager() :
+        window(new sf::RenderWindow(sf::VideoMode(WIDTH, HEIGHT), "JOGO")),
+        font(new sf::Font())
+    {
+        if (!font->loadFromFile(FONT_PATH))
+        {
+            std::cout << "Error loading Font!" << std::endl;
+            exit(1);
+        }
+    }
+
+
+    GraphicsManager::~GraphicsManager()
+    {
+        delete window;
+        window = NULL;
+        instance = NULL;
+    }
+
+
+    bool GraphicsManager::isWindowOpen()
+    {
+        return (window->isOpen());
+    }
+
+
+    void GraphicsManager::clear()
+    {
+        if (isWindowOpen())
+            window->clear();
+    }
+
+
+    void GraphicsManager::close()
+    {
+        if (isWindowOpen())
+            window->close();
+    }
+
+    /* Updates the window onto the screen */
+    void GraphicsManager::display()
+    {
+        if (isWindowOpen())
+            window->display();
+    }
+
+
+    sf::RenderWindow* GraphicsManager::getWindow()
+    {
+        return window;
+    }
+
+
+    void GraphicsManager::updateDeltaTime()
+    {
+        deltaTime = clock.restart().asSeconds();
+        if(deltaTime > 0.1f)
+            deltaTime = 0.1f;
+
+    }
+
+
+    const float GraphicsManager::getDeltaTime()
+    {
+        return deltaTime;
+    }
+
+
+    /* Returns a font pointer to be used by texts. */
+    sf::Font* GraphicsManager::getFont() const
+    {
+        return font;
+    }
+
+}// namespace Managers
+
