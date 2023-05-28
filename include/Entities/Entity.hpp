7929f8ec56ee165495f9991d42043c82a9deaff0
MuriloUnten <murilokenjiunten@gmail.com>
2023-05-28 17:31:24 -0300

    entity methods

--- include/Entities/Entity.hpp
+++ include/Entities/Entity.hpp
@@ -7,13 +7,15 @@ namespace Entities
 {
     class Entity
     {
-    private:
+    protected:
         static Managers::GraphicsManager* graphics;
+
         sf::Sprite sprite;
         ID::EntityID id;
 
-        sf::Vector2f vel;
-        sf::Vector2f acc;
+        sf::Vector2f position;
+        sf::Vector2f velocity;
+        sf::Vector2f acceleration;
 
         bool executable;
 
@@ -23,5 +25,16 @@ namespace Entities
 
         virtual void draw();
         virtual void execute() = 0;
+
+        const ID::EntityID getID() const;
+        
+        const sf::Vector2f getPosition() const;
+        const sf::Vector2f getVelocity() const;
+        const sf::Vector2f getAcceleration() const;
+
+        void updatePosition(float dt);
+        void updateVelocity(float dt);
+
+        const bool isExecutable() const;
     };
 }// namespace Entities
--- src/Entities/Entity.cpp
+++ src/Entities/Entity.cpp
@@ -19,4 +19,45 @@ namespace Entities {
     {
         graphics->draw(sprite);
     }
+
+
+    const ID::EntityID Entity::getID() const
+    {
+        return id;
+    }
+    
+    const sf::Vector2f Entity::getPosition() const
+    {
+        return position;
+    }
+
+
+    const sf::Vector2f Entity::getVelocity() const
+    {
+        return velocity;
+    }
+
+
+    const sf::Vector2f Entity::getAcceleration() const
+    {
+        return acceleration;
+    }
+
+
+    void Entity::updatePosition(float dt)
+    {
+        position += velocity * dt;
+    }
+
+
+    void Entity::updateVelocity(float dt)
+    {
+        velocity += acceleration * dt;
+    }
+
+
+    const bool Entity::isExecutable() const
+    {
+        return executable;
+    }
 }// namespace Entities
