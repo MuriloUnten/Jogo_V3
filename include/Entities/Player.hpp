ae80b2fec5dd8d008392040502b0e985c98dd24a
MuriloUnten <murilokenjiunten@gmail.com>
2023-05-31 23:20:30 -0300

    minor changes to player velocity and acceleration macros

--- include/Entities/Player.hpp
+++ include/Entities/Player.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
-#define PLAYER_ATTACK_COOLDOWN 1
+#define PLAYER_ATTACK_COOLDOWN 0.5f
 #define PLAYER_SIZE sf::Vector2f(56.0f, 224.0f)
 #define N_PROJECTILES 10
-#define PLAYER_ACCELERATION_X 800
-#define PLAYER_BREAK_COEFFICIENT 5
-#define PLAYER_MAX_VELOCITY sf::Vector2f(500.0f, 500.0f)
+#define PLAYER_ACCELERATION_X 2000
+#define PLAYER_BREAK_COEFFICIENT 10
+#define PLAYER_MAX_VELOCITY sf::Vector2f(600.0f, 500.0f)
 
 #define PLAYER_TEXTURE_PATH "../assets/player.png"
 
